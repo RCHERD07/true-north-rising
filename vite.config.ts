3d8f7076c6e7f79467fee89aff32f312e27fc628
Richard C <rcarsillas3@gatech.edu>
2026-03-21 15:30:22 -0700

    Added cloudflare for vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isNetlify = process.env.NETLIFY === "true";
+const isCloudflarePages = process.env.CF_PAGES === "1";
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NETLIFY === "true" ? "/" : "/true-north-rising/",
+  base: isNetlify || isCloudflarePages ? "/" : "/true-north-rising/",
 });
